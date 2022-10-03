ed38167c993358c70e845803e8c5a23f721ac708
JD <jdakemp@gmail.com>
2022-10-03 13:26:08 -0400

    Finshed the Higher / Lower Guessing Game

--- src/main/java/com/coderscampus/assignment2/HigherLowerGuessingApp.java
+++ src/main/java/com/coderscampus/assignment2/HigherLowerGuessingApp.java
@@ -5,36 +5,38 @@ import java.util.Scanner;
 
 public class HigherLowerGuessingApp {
 
-	public static void main(String[] args) { 
+	private void getInput() {
 		
-		System.out.println(" ****** WELCOME TO THE GUESS GAME ******"); 
-		System.out.println(" You have to Guess a number between 0 & 100 to WIN!!! "); 
-		System.out.println(" you only have 5 tries so guess wisley ") ; 
-		System.out.println( "                    LETS BEGIN ");
-		
-		Random rand = new Random(); 
+		Random rand = new Random();
 		int r = rand.nextInt(101);
 		
-		Scanner scan = new Scanner(System.in); 
-		System.out.println(" Pick a number between 1 and 100"); 
+		Scanner scan = new Scanner(System.in);
+		System.out.println(" Pick a number between 1 and 100");
 		
-		int inputInt = Integer.parseInt(getInput(scan, " Enter an a Number "));
-		
-		private String getInput(Scanner in, String prompt) {
-			System.out.print(prompt); // Tell user what to input 
-			int numberInput; 
-			while (numberInput != r) {
-				
+		int numberInput = scan.nextInt();
+		int counter = 0;
+
+		while (numberInput != r) {
+			numberInput = scan.nextInt();
+			counter = counter++;
+
+			if (numberInput < r) {
+				System.out.println(" Please pick a higher number " );
+			} else if (numberInput > r) {
+				System.out.println(" Please pick a lower number " );
+			} else if (numberInput == r) {
+				System.out.println(" Congrats you guess it correctly! ");
+				break;
+			} else if (counter < 5) {
+				System.out.println(" YOU LOSE! only get 5 tries .... ");
+				break;
 			}
 		}
-		
-//		if (firstGuess < 0 || firstGuess > 100) {
-//			System.out.println(" Your guess is not between 1 and 100, please try again ");
-//		} else if(firstGuess == r ){
-//			System.out.println(" You guess it!! ********* Congrats you won ********* ");
-//		} else if( firstGuess < r ) {
-//			System.out.println(" Please pick a higher number "); 
-//		} else if ()
 	}
 
- }
+	public static void main(String[] args) {
+
+		new HigherLowerGuessingApp().getInput();
+
+	}
+}
