d55ef81b04c2e947492e61c57cc307b3c97eb844
JD <jdakemp@gmail.com>
2022-10-08 11:51:58 -0400

    fix the errors, application only allows user to guess 5 times and then stops now

--- src/main/java/com/coderscampus/assignment2/HigherLowerGuessingApp.java
+++ src/main/java/com/coderscampus/assignment2/HigherLowerGuessingApp.java
@@ -13,21 +13,28 @@ public class HigherLowerGuessingApp {
 		Scanner scan = new Scanner(System.in);
 		System.out.println(" Pick a number between 1 and 100");
 		
-		int numberInput = scan.nextInt();
+		int numberInput = 0;
 		int counter = 0;
+		
+		numberInput = scan.nextInt(); 
 
 		while (numberInput != r) {
-			numberInput = scan.nextInt();
-			counter = counter++;
-
+			
+			
 			if (numberInput < r) {
+				counter++;
 				System.out.println(" Please pick a higher number " );
+				numberInput = scan.nextInt();  
 			} else if (numberInput > r) {
+				counter++;
 				System.out.println(" Please pick a lower number " );
+				numberInput = scan.nextInt(); 
 			} else if (numberInput == r) {
 				System.out.println(" Congrats you guess it correctly! ");
 				break;
-			} else if (counter < 5) {
+			} 
+			
+			if (counter > 3 ) {
 				System.out.println(" YOU LOSE! only get 5 tries .... ");
 				break;
 			}
