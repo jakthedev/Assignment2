b8f8459177b42af43ae9b5798cd1a4f12b0b65e7
JD <jdakemp@gmail.com>
2022-10-27 19:04:53 -0400

    Just completed the assignment it runs doing all test cases

--- src/main/java/com/coderscampus/assignment2/HigherLowerGuessingApp.java
+++ src/main/java/com/coderscampus/assignment2/HigherLowerGuessingApp.java
@@ -8,7 +8,8 @@ public class HigherLowerGuessingApp {
 	private void getInput() {
 		
 		Random rand = new Random();
-		int r = rand.nextInt(100);
+		int r = rand.nextInt(100); 
+		System.out.println(r);
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println(" Pick a number between 1 and 100");
@@ -18,32 +19,40 @@ public class HigherLowerGuessingApp {
 		
 		numberInput = scan.nextInt(); 
 
-		while (numberInput != r) {
+		//if (numberInput != r) {
 			
-			while (numberInput <= 0 || numberInput >= 101) {
+		
+			while (counter < 4) {
+				
+				if (numberInput <= 0 || numberInput >= 101) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
 				numberInput = scan.nextInt();
-			} 
-			
-			 if (numberInput < r) {
-				counter++;
+				}
+				
+				else if (numberInput < r) {
 				System.out.println(" Please pick a higher number " );
 				numberInput = scan.nextInt();  
-			} else if (numberInput > r) {
 				counter++;
+				
+			} else if (numberInput > r) {
 				System.out.println(" Please pick a lower number " );
 				numberInput = scan.nextInt(); 
-			} else if (numberInput == r) {
+				counter++;
+				
+			}  else if (numberInput == r) {
 				System.out.println(" Congrats you guess it correctly! ");
-				break;
+				System.exit(0);
 			} 
+		}
 			
-			if (counter > 4 ) {
-				System.out.println(" YOU LOSE! only get 5 tries .... ");
-				break;
+			if (numberInput == r) {
+				System.out.println(" Congrats you guess it correctly! ");
+			}
+			
+			if (counter == 4 && numberInput != r ) {
+					System.out.println(" YOU LOSE! only get 5 tries .... ");
 			}
 		}
-	}
 
 	public static void main(String[] args) {
 
